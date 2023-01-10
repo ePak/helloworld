cc7c4543cb4abbf5b8530319fb52a1985adc6bf5
Eric Pak <eric.pak@reece.com.au>
2023-01-10 14:44:57 +1100

    add setup.py and hello()

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='helloworld',
+      version='0.1',
+      description='Hello world',
+      url='http://github.com/reecegroup-databricks/helloworld',
+      author='Eric Pak',
+      author_email='eric.pak@reece.com.au',
+      license='MIT',
+      packages=['helloworld'],
+      zip_safe=False) 
